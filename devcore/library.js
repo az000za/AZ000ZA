8df50b2e6ed72fbd80958bd5ee539eb043296d3a
az000za <150498639+az000za@users.noreply.github.com>
2023-11-20 11:40:46 -0500

    Update library.js

--- devcore/library.js
+++ devcore/library.js
@@ -1,28 +1,27 @@
-class Library {
-    constructor(name, description, modules) {
-      this.name = name;
-      this.description = description;
-      this.modules = modules;
-    }
-  
-    getName() {
-      return this.name;
-    }
-  
-    getDescription() {
-      return this.description;
-    }
-  
-    getModules() {
-      return this.modules;
-    }
-  
-    addModule(module) {
-      this.modules.push(module);
-    }
-  
-    removeModule(module) {
-      this.modules.splice(this.modules.indexOf(module), 1);
-    }
-  }
-  
+<html>
+    <script type="text/javascript"></script>
+    <script type="text/javascript"></script>
+        class Library {
+            constructor(name, description, modules) {
+              this.name = name;
+              this.description = description;
+              this.modules = modules;
+            }
+            getName() {
+              return this.name;
+            }
+            getDescription() {
+              return this.description;
+            }
+            getModules() {
+              return this.modules;
+            }
+            addModule(module) {
+              this.modules.push(module);
+            }
+            removeModule(module) {
+              this.modules.splice(this.modules.indexOf(module), 1);
+            }
+          }
+    </script>
+</html>  
