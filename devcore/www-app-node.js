101d26c5ada2e1faf0c097b385b17be1489b7372
az000za <150498639+az000za@users.noreply.github.com>
2023-11-20 16:17:14 -0500

    Update www-app-node.js

--- devcore/www-app-node.js
+++ devcore/www-app-node.js
@@ -14,32 +14,55 @@ console.log(`
   why?
 
   because html is advanced and simple.
+  html is customizable in a sense you can just create your
+  own html tags and supported attributes
+  and there is no reason to use frameworks, just a design a different
+  perspective of how to use html.
 
   html is the highest level of a programming language and the only thing
-
   higher than it is Natural Language.
-
   and for this one main reason.
-
-  <script src="<any url>"</script>
-
-  it can load data from a route.
+  
+  <script type="<file type>" src="<any url>"</script>
+  
+  it can load any data from a route that the browser supports.
 
   meaning I can just import code not only from my filesystem
   but whatever is available on the web.
 
-  imagine how much simpler everything would be if we could just code in html.
-  
-  Why not use React or Angular?!?!?
+  boom there you have it, no more complexity of dealing with low level or backend
+  languages. and why do they call it node.js if you still have
+  to go through a few steps to put your code on the internet.
+  html just needs a config form script that auto deploys everything you work on.
+  such as 
+  <script type="" src="./DeployThisApp.js"></script>
 
-  * Because those codebases have a versioning dependency nightmare.
-  * People learn html first
+  imagine how much simpler everything would be if we could just code in html.
+
+  Why not use React or Angular?!?!?
+  * People learn html as there first programming language.
+  * Because those frameworks have a versioning dependency nightmare.
+  * Because there code goes straight to html
   * Frameworks are just hype trends ... Consider wwwnode to just be a suggested code design pattern to solve lots of headaches
   * Frameworks require people to go out of there way to learn especially for new versions.
-  * html just needs more support
   * The end goal of a framework is to make reusable components that have no dependencies
   * Meaning the end goal is just to write html so that anyone can contribute to the web easily
+  * This makes app development a web first approach and most applications are just text documents
+  * So most things should just be written in component called a simple html document
+  * that uses the url router
+  * HTML is supported by the browser so we use html
 
+  When you think of HTML
+    think of a language that supports everything
+    because the browser supports whatever extensions people add to it.
+    Especially other programming languages.
+
+  HTML can even be used for storing structured data or even used
+  for your database models.
+
+  And heres even the best part about html.
+  It's already in an app form so every function we
+  write comes with a generic interface at the minimum.
 `);
 
 const args = process.argv.slice(2);
@@ -51,3 +74,21 @@ const http = require("http");
 
 // start server & open default browser
 
+const { exec } = require('child_process');
+const url = argFileName;
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, World!</h1>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+
+  setTimeOut(exec(`open ${url}`), 2000);
+});
