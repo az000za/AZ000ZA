03ea86eb3fe3b636ea42dbefc8c636235d329ec4
az000za <150498639+az000za@users.noreply.github.com>
2023-11-20 12:34:28 -0500

    Update and rename route.js to route.html

--- ui/route.js
+++
@@ -1,9 +0,0 @@
-class Route {
-    constructor(obj){
-        // super();
-        this.path = obj.path;
-        this.guard = obj.guard;
-        this.initModule = obj.initModule;
-        this.populateUINodes = obj.populateUINodes
-    }
-}
