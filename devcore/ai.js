1b608cd452cb8f60364dddf5f822b9ad5f1212f7
az000za <150498639+az000za@users.noreply.github.com>
2023-11-20 12:06:27 -0500

    Update ai.js

---
+++ devcore/ai.js
@@ -0,0 +1,11 @@
+<html>
+    <body id="root"></body>
+    <script type="text/javascript" src="./ui/data.js"></script>
+    <script type="text/javascript" filename="AI.js">
+        class AI {
+          constructor(){
+            console.log("class AI unprogrammed");
+          }
+        }
+    </script>
+  </html>
