3bd78c75cc5ac18c17a97dbe62e73442edc12020
az000za <150498639+az000za@users.noreply.github.com>
2023-11-30 18:43:14 -0500

    Add files via upload

--- DevCore/importHTMLAPP.js
+++ DevCore/importHTMLAPP.js
@@ -1,18 +1,37 @@
-function importHTMLFiles(filePaths) {
+function importHTMLAPP(filePaths) {
+  const Apps = {};
     for (const filePath of filePaths) {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', filePath, true);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          const html = xhr.responseText;
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(html, 'text/html');
-          const body = doc.body.cloneNode(true);
-          document.body.appendChild(body);
-        } else {
-          console.error('Error importing HTML file:', filePath);
-        }
-      };
-      xhr.send();
+      // const xhr = new XMLHttpRequest();
+      // xhr.open('GET', filePath, true);
+      // xhr.onload = function() {
+      //   if (xhr.status === 200) {
+      //     const html = xhr.responseText;
+      //     const parser = new DOMParser();
+      //     const doc = parser.parseFromString(html, 'text/html');
+      //     const body = doc.body.cloneNode(true);
+      //     document.body.appendChild(body);
+
+      //     console.log("imported ", filePath);
+      //     console.log(html);
+
+      //     Apps[AppName] = function(){
+      //       return html;
+      //     }
+
+      //   } else {
+      //     console.error('Error importing HTML file:', filePath);
+      //   }
+      // };
+      // xhr.send();
+      // const fileName = document.URL.split('/').pop();
+      // alert(fileName);
+      const url = filePath;
+      fetch(url, {
+        mode: "no-cors"
+      })
+      .then(response => response.text())
+      .then(data => console.log('data', data))
+      .catch(error => console.error(error));
     }
+    return Apps;
   }
