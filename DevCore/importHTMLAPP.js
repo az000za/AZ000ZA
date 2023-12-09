6cfdf4da9c4c12784eeb89e9e86e7c310d340e7e
az000za <150498639+az000za@users.noreply.github.com>
2023-12-08 21:02:10 -0500

    Update importHTMLAPP.js

--- DevCore/importHTMLAPP.js
+++ DevCore/importHTMLAPP.js
@@ -37,37 +37,45 @@ function importHTMLAPP(filePaths) {
   `);
   const Apps = {};
     for (const filePath of filePaths) {
-      // const xhr = new XMLHttpRequest();
-      // xhr.open('GET', filePath, true);
-      // xhr.onload = function() {
-      //   if (xhr.status === 200) {
-      //     const html = xhr.responseText;
-      //     const parser = new DOMParser();
-      //     const doc = parser.parseFromString(html, 'text/html');
-      //     const body = doc.body.cloneNode(true);
-      //     document.body.appendChild(body);
-
-      //     console.log("imported ", filePath);
-      //     console.log(html);
-
-      //     Apps[AppName] = function(){
-      //       return html;
-      //     }
-
-      //   } else {
-      //     console.error('Error importing HTML file:', filePath);
-      //   }
-      // };
-      // xhr.send();
-      // const fileName = document.URL.split('/').pop();
-      // alert(fileName);
+      const fileName = document.URL.split('/').pop();
       const url = filePath;
       fetch(url, {
         mode: "no-cors"
       })
       .then(response => response.text())
-      .then(data => console.log('data', data))
-      .catch(error => console.error(error));
+      .then(data => {        
+          const html = data;
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, 'text/html');
+          const body = doc.body.cloneNode(true);
+          console.log("imported ", filePath);
+          console.log(html);
+
+          const AppName = fileName.split(".").slice().shift();
+
+          Apps[AppName] = function(){                        
+            return new class _ {
+              peerID = 0;  // needs to generated
+              html = html;
+              css = css;
+              javascript = javascript;
+              constructor(){
+                
+              }
+              renderGenericUI(){
+                console.error("renderGenericUI unprogrammed");
+              }
+              renderCustomUI(){
+                console.error("renderCustomUI unprogrammed");
+              }
+              update(){
+                console.error("update unprogrammed");
+              }
+            }
+          }
+        
+      })
+      .catch(error => console.error('Error importing HTML file:', filePath));
     }
     return Apps;
   }
